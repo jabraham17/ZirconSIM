96d73c688d5bda2239357dc8f5b50fd3882a44e0
jacob-abraham <jacob.r.abraham@gmail.com>
2022-10-24 19:50:58 -0700

    fix formatting script

--- format.sh
+++ format.sh
@@ -3,9 +3,9 @@
 CF=clang-format
 CF=../llvm/llvm-project/build/bin/clang-format
 
-FILES=("src/*.c" "src/*.cpp" "src/*.cc" "src/*.h" "src/*.inc")
+FILES=("./src/*.c" "./src/*.cpp" "./src/*.cc" "./src/*.h" "./src/*.inc")
 set -e
 
 for s in "${FILES[@]}"; do
-    (set -x && find . -wholename $s -type f -exec $CF -i {} +)
+    (set -x && find . -ipath $s -type f -exec $CF -i {} +)
 done
