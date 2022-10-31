6d27a73371f0d8cc50dcdc1a1e61285350b45e10
jacob-abraham <jacob.r.abraham@gmail.com>
2022-10-31 13:43:04 -0700

    improve wasm

--- src/cpu/syscall/syscall.cpp
+++ src/cpu/syscall/syscall.cpp
@@ -10,6 +10,10 @@
 #include <time.h>
 #include <unistd.h>
 
+#ifdef __EMSCRIPTEN__
+#include <syscall_arch.h>
+#endif
+
 namespace sys {
 
 namespace internal {
@@ -85,17 +89,18 @@ void emulate(cpu::HartState& hs) {
           [sys] "r"(x86_64_syscall_number)
         : "rax", "rdi", "rsi", "rdx", "r10", "r8", "r9");
 #else
-    // #ifdef __EMSCRIPTEN__
-    //     uint64_t arg0 = hs.rf.GPR[10];
-    // uint64_t arg1 = hs.rf.GPR[11];
-    // uint64_t arg2 = hs.rf.GPR[12];
-    // uint64_t arg3 = hs.rf.GPR[13];
-    // uint64_t arg4 = hs.rf.GPR[14];
-    // uint64_t arg5 = hs.rf.GPR[15];
-    // result = syscall(x86_64_syscall_number, arg0, arg1, arg2, arg3, arg4,
-    // arg5); #else
+//     #ifdef __EMSCRIPTEN__
+//     uint64_t arg0 = hs.rf.GPR[10];
+//     uint64_t arg1 = hs.rf.GPR[11];
+//     uint64_t arg2 = hs.rf.GPR[12];
+//     uint64_t arg3 = hs.rf.GPR[13];
+//     uint64_t arg4 = hs.rf.GPR[14];
+//     uint64_t arg5 = hs.rf.GPR[15];
+
+//     result = __syscall_emscripten(x86_64_syscall_number, arg0, arg1, arg2, arg3, arg4,
+//     arg5);
+//     #else
     throw SyscallUnimplementedException(riscv64_syscall_number);
-// assert(0 && "Not supported\n");
 // #endif
 #endif
     hs.rf.GPR[10] = result;
--- src/zircon-wasm/main.cpp
+++ src/zircon-wasm/main.cpp
@@ -1,6 +1,8 @@
 
 
+#include "common/format.h"
 #include "cpu/cpu.h"
+#include "cpu/isa/inst.h"
 #include "elf/elf.h"
 #include "mem/memory-image.h"
 #include <fstream>
@@ -17,15 +19,39 @@ void emulate_impl(std::string filename) {
         return;
     }
 
-    TraceMode traces = TraceMode::NONE;
-    bool stats = false;
-
     elf::File f(std::move(is));
-    mem::MemoryImage memimg(0xF0000, traces);
+    mem::MemoryImage memimg(0xF0000);
     f.buildMemoryImage(memimg);
     auto start = f.getStartAddress();
 
-    cpu::Hart hart(memimg, traces, stats);
+    cpu::Hart hart(memimg);
+
+    std::ostream* inst_log = &std::cout;
+    std::ostream* reg_log = &std::cout;
+
+    hart.addBeforeExecuteListener([inst_log](cpu::HartState& hs) {
+        auto inst = hs.getInstWord();
+        *inst_log << "PC[" << common::Format::doubleword << hs.pc
+                  << "] = " << common::Format::word << inst << "; "
+                  << isa::inst::disassemble(inst, hs.pc) << std::endl;
+    });
+    hart.addRegisterReadListener(
+        [reg_log](std::string classname, uint64_t idx, uint64_t value) {
+            *reg_log << "RD " << classname << "[" << common::Format::dec << idx
+                     << "] = " << common::Format::doubleword << (uint64_t)value
+                     << std::endl;
+        });
+    hart.addRegisterWriteListener([reg_log](
+                                      std::string classname,
+                                      uint64_t idx,
+                                      uint64_t value,
+                                      uint64_t oldvalue) {
+        *reg_log << "WR " << classname << "[" << common::Format::dec << idx
+                 << "] = " << common::Format::doubleword << (uint64_t)value
+                 << "; OLD VALUE = " << common::Format::doubleword << oldvalue
+                 << std::endl;
+    });
+
     hart.init();
     hart.execute(start);
 }
