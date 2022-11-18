69a723c781c47d27c1f1d6089a9f220e653ec139
jacob-abraham <jacob.r.abraham@gmail.com>
2022-11-18 15:26:47 -0700

    improve env test

--- test/envp.c
+++ test/envp.c
@@ -12,17 +12,21 @@ int main(int argc, char** argv, char** envp) {
     }
     printf("AFTER ARGS %p %p\n", argv + i, argv[i]);
 
+    printf("\n");
+
     printf("ENV %p\n", envp);
     for(i = 0; envp[i] != 0; i++) {
         printf("%p %p %s\n", envp + i, envp[i], envp[i]);
     }
     printf("AFTER ENV %p %p\n", envp + i, envp[i]);
 
+    printf("\n");
+
     for(i = 0; envp[i]; i++)
         ;
     long* auxv = (void*)(envp + i + 1);
     for(; auxv[0] != AT_NULL; auxv += 2) {
-        printf("%d is: 0x%x\n", auxv[0], auxv[1]);
+        printf("%ld is: 0x%lx\n", auxv[0], auxv[1]);
     }
     return 0;
 }
