10c039effbe091ff97d235b0a54dd36f7a2c7d43
jacob-abraham <jacob.r.abraham@gmail.com>
2022-11-20 12:52:36 -0700

    fix utype imm and lui and auipc both should have been sign extending 32 bits, not 20. and the two instructions should have used the sign extended form double BUG

--- src/cpu/isa/inst.cpp
+++ src/cpu/isa/inst.cpp
@@ -82,6 +82,17 @@ std::string disassemble(uint32_t bits, uint32_t pc, bool color) {
     Opcode op = decodeInstruction(bits);
     switch(op) {
         default: break;
+        case Opcode::rv32i_auipc: {
+            std::stringstream ss;
+            ss << internal::colorOpcode(color) << Opcode::getNiceName(op)
+               << internal::colorReset(color) << " "
+               << internal::colorReg(color) << "x" << instruction::getRd(bits)
+               << internal::colorReset(color) << ", "
+               << internal::colorNumber(color) <<  "0x" << std::hex
+               << (instruction::getUTypeImm(bits) >> 12)
+               << internal::colorReset(color);
+            return ss.str();
+        }
         case Opcode::rv32i_lb:
         case Opcode::rv32i_lh:
         case Opcode::rv32i_lw:
