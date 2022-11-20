353402149b3d90b656ac186f630a280654aea028
jacob-abraham <jacob.r.abraham@gmail.com>
2022-11-20 11:22:35 -0700

    improve test case

--- test/envp.c
+++ test/envp.c
@@ -15,18 +15,21 @@ int main(int argc, char** argv, char** envp) {
     printf("\n");
 
     printf("ENV %p\n", envp);
-    for(i = 0; envp[i] != 0; i++) {
-        printf("%p %p %s\n", envp + i, envp[i], envp[i]);
-    }
-    printf("AFTER ENV %p %p\n", envp + i, envp[i]);
+    if(envp) {
+        for(i = 0; envp[i] != 0; i++) {
+            printf("%p %p %s\n", envp + i, envp[i], envp[i]);
+        }
+        printf("AFTER ENV %p %p\n", envp + i, envp[i]);
+    
+        printf("\n");
 
-    printf("\n");
-
-    for(i = 0; envp[i]; i++)
-        ;
-    long* auxv = (void*)(envp + i + 1);
-    for(; auxv[0] != AT_NULL; auxv += 2) {
-        printf("%ld is: 0x%lx\n", auxv[0], auxv[1]);
+        for(i = 0; envp[i]; i++)
+            ;
+        long* auxv = (void*)(envp + i + 1);
+        for(; auxv[0] != AT_NULL; auxv += 2) {
+            printf("%ld is: 0x%lx\n", auxv[0], auxv[1]);
+        }
     }
+
     return 0;
 }
