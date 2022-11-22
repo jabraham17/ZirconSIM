a60fc643dc4c68a0677e68ed412c55f20ecbaf21
jacob-abraham <jacob.r.abraham@gmail.com>
2022-11-22 13:28:55 -0700

    update env

--- test/env_vars.sh
+++ test/env_vars.sh
@@ -1,13 +1,33 @@
+realpath() {
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+}
+SCRIPT_NAME=
+if [ -z $BASH_SOURCE ]; then
+    SCRIPT_NAME=$0
+else
+    SCRIPT_NAME=${BASH_SOURCE[0]}
+fi
+SCRIPT_DIR=$(realpath "$(dirname "$SCRIPT_NAME")")
 
-export RVGCC_SOURCE=/home/jabraham/Development/tools/riscv-gnu-toolchain
 
-export RV64IMA_ELF=/home/jabraham/Development/ZirconSIM/toolchains/rv64ima
-export RV64IMA_ELF_DEBUG=/home/jabraham/Development/ZirconSIM/toolchains/rv64ima-debug
+ZIRCON_HOME=$SCRIPT_DIR/..
 
-export RV64IMA_LINUX=/home/jabraham/Development/ZirconSIM/toolchains/rv64ima-linux
-export RV64IMA_LINUX_DEBUG=/home/jabraham/Development/ZirconSIM/toolchains/rv64ima-linux-debug
+export RV64IMA_ELF=$ZIRCON_HOME/toolchains/rv64ima
+export RV64IMA_ELF_DEBUG=$ZIRCON_HOME/toolchains/rv64ima-debug
 
-export RV64IMA_MUSL=/home/jabraham/Development/ZirconSIM/toolchains/rv64ima-musl
-export RV64IMA_MUSL_DEBUG=/home/jabraham/Development/ZirconSIM/toolchains/rv64ima-musl-debug
+export RV64IMA_LINUX=$ZIRCON_HOME/toolchains/rv64ima-linux
+export RV64IMA_LINUX_DEBUG=$ZIRCON_HOME/toolchains/rv64ima-linux-debug
+
+export RV64IMA_MUSL=$ZIRCON_HOME/toolchains/rv64ima-musl
+export RV64IMA_MUSL_DEBUG=$ZIRCON_HOME/toolchains/rv64ima-musl-debug
 
 
