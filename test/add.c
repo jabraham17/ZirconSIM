60941719c552cd2299e08ff19741fef44cdac871
jabraham17 <jacob.r.abraham@gmail.com>
2022-12-29 09:35:32 -0700

    improve test case

--- test/add.c
+++ test/add.c
@@ -2,3 +2,10 @@
 /*clang --target=riscv64 -march=rv64i -O3 -c test/add.c -o test/add.o &&
  * llvm-objcopy -O binary --only-section=.text test/add.o test/add.bin */
 long add(long a, long b) { return a + b; }
+
+
+long data;
+long data2;
+long add2(long a, long b) { return data + data2 + a + b; }
+
+int main() {return 0;}
