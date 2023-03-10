2c59b58a4739826553ebb0005065148351f350f3
jabraham17 <jademabraham17@gmail.com>
2023-03-09 21:47:46 -0800

    add main for console changes
    Signed-off-by: jabraham17 <jademabraham17@gmail.com>

--- src/zircon/main.cpp
+++ src/zircon/main.cpp
@@ -6,6 +6,7 @@
 #include "hart/hart.h"
 #include "ishell/parser/parser.h"
 #include "trace/stats.h"
+#include "ishell/repl.h"
 
 int main(int argc, const char** argv, const char** envp) {
 
@@ -31,49 +32,59 @@ int main(int argc, const char** argv, const char** envp) {
             [&stats](hart::HartState& hs) { stats.count(hs); });
     }
 
-    auto t = std::thread([&hart]() {
-        auto parser = ishell::parser::Parser();
-        while(1) {
-            // hart.hs().waitForExecutionStateChange();
-            if(hart.hs().isPaused()) {
-                std::cout.flush();
-                std::cout << "> ";
-                std::string input;
-                std::getline(std::cin, input);
+    // auto t = std::thread([&hart]() {
+    //     auto parser = ishell::parser::Parser();
+    //     while(1) {
+    //         // hart.hs().waitForExecutionStateChange();
+    //         if(hart.hs().isPaused()) {
+    //             std::cout.flush();
+    //             std::cout << "> ";
+    //             std::string input;
+    //             std::getline(std::cin, input);
 
-                try {
-                    auto control = parser.parse(input);
-                    control->setHS(&hart.hs());
-                    if(auto command =
-                           std::dynamic_pointer_cast<command::Command>(
-                               control)) {
-                        command->doit(&std::cout);
-                    } else {
-                        std::cerr
-                            << "Only COMMANDs are supported at this time\n";
-                    }
-                } catch(const ishell::parser::ParseException& pe) {
-                    std::cerr << "Invalid command\n";
-                }
+    //             try {
+    //                 auto control = parser.parse(input);
+    //                 control->setHS(&hart.hs());
+    //                 if(auto command =
+    //                        std::dynamic_pointer_cast<command::Command>(
+    //                            control)) {
+    //                     command->doit(&std::cout);
+    //                 } else {
+    //                     std::cerr
+    //                         << "Only COMMANDs are supported at this time\n";
+    //                 }
+    //             } catch(const ishell::parser::ParseException& pe) {
+    //                 std::cerr << "Invalid command\n";
+    //             }
 
-            } else if(
-                hart.hs().getExecutionState() ==
-                    hart::ExecutionState::STOPPED ||
-                hart.hs().getExecutionState() ==
-                    hart::ExecutionState::INVALID_STATE) {
-                break;
-            }
-        }
-    });
+    //         } else if(
+    //             hart.hs().getExecutionState() ==
+    //                 hart::ExecutionState::STOPPED ||
+    //             hart.hs().getExecutionState() ==
+    //                 hart::ExecutionState::INVALID_STATE) {
+    //             break;
+    //         }
+    //     }
+    // });
 
     hart.init(args.getArgV(), args.getEnvVars());
-    hart.hs().start(start);
-    // TODO: very temporary, race condition
+    hart.hs().setPC(start);
+    
+    ishell::Repl repl(&hart.hs());
+
     if(args.accessRawArguments().get<bool>("--start-paused")) {
         hart.hs().pause();
     }
+    else {
+        hart.hs().start();
+    }
+    hart.startExecution();
+    repl.run();
+
+
     hart.wait_till_done();
-    t.join();
+    repl.wait_till_done();
+    // t.join();
 
     if(args.accessRawArguments().get<bool>("--stats")) {
         std::cout << stats.dump() << std::endl;
