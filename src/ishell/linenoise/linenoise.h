241479fe2e0752a6bfb3d79f919b671811fea3c6
jabraham17 <jademabraham17@gmail.com>
2023-03-11 17:47:29 -0800

    add args to callbacks in  linenoise and format
    Signed-off-by: jabraham17 <jademabraham17@gmail.com>

--- src/ishell/linenoise/linenoise.h
+++ src/ishell/linenoise/linenoise.h
@@ -40,28 +40,37 @@
 #define __LINENOISE_H
 
 #ifdef __cplusplus
+#include <cstddef>
+#else
+#include <stddef.h>
+#endif
+
+
+#ifdef __cplusplus
 extern "C" {
 #endif
 
+
 typedef struct linenoiseCompletions {
-  size_t len;
-  char **cvec;
+    size_t len;
+    char** cvec;
 } linenoiseCompletions;
 
-typedef void(linenoiseCompletionCallback)(const char *, linenoiseCompletions *);
-typedef char*(linenoiseHintsCallback)(const char *, int *color, int *bold);
-typedef void(linenoiseFreeHintsCallback)(void *);
-void linenoiseSetCompletionCallback(linenoiseCompletionCallback *);
-void linenoiseSetHintsCallback(linenoiseHintsCallback *);
-void linenoiseSetFreeHintsCallback(linenoiseFreeHintsCallback *);
-void linenoiseAddCompletion(linenoiseCompletions *, const char *);
+typedef void(
+    linenoiseCompletionCallback)(void*, const char*, linenoiseCompletions*);
+typedef char*(linenoiseHintsCallback)(void*, const char*, int* color, int* bold);
+typedef void(linenoiseFreeHintsCallback)(void*);
+void linenoiseSetCompletionCallback(linenoiseCompletionCallback*, void*);
+void linenoiseSetHintsCallback(linenoiseHintsCallback*, void*);
+void linenoiseSetFreeHintsCallback(linenoiseFreeHintsCallback*);
+void linenoiseAddCompletion(linenoiseCompletions*, const char*);
 
-char *linenoise(const char *prompt);
-void linenoiseFree(void *ptr);
-int linenoiseHistoryAdd(const char *line);
+char* linenoise(const char* prompt);
+void linenoiseFree(void* ptr);
+int linenoiseHistoryAdd(const char* line);
 int linenoiseHistorySetMaxLen(int len);
-int linenoiseHistorySave(const char *filename);
-int linenoiseHistoryLoad(const char *filename);
+int linenoiseHistorySave(const char* filename);
+int linenoiseHistoryLoad(const char* filename);
 void linenoiseClearScreen(void);
 void linenoiseSetMultiLine(int ml);
 void linenoisePrintKeyCodes(void);
